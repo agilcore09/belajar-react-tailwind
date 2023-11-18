164c139e491c0c5dff4027799be006f7b0c91c20
unknown <agiljibrin009@gmail.com>
2023-11-18 21:44:28 +0800

    sudah ada react dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './Login/Login';
+import Register from './Login/Register';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+
+  {
+    path: '/register',
+    element: <Register />
+  },
+
+  {
+    path: '*',
+    element: <h1>Telang</h1>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
